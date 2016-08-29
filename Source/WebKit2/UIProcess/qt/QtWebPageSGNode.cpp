ed491da9eeaf2b9c1e63ea32e2feea1aee295a0b
Konstantin Tokarev <annulen@yandex.ru>
2016-08-29 20:10:56 +0300

    Replaced Q_DECL_OVERRIDE with override.
    Change-Id: I95d738bcc42f9e6c2c4464e662df1996a27fdb70

--- Source/WebKit2/UIProcess/qt/QtWebPageSGNode.cpp
+++ Source/WebKit2/UIProcess/qt/QtWebPageSGNode.cpp
@@ -62,12 +62,12 @@ public:
 
 #else
 
-    StateFlags changedStates() Q_DECL_OVERRIDE
+    StateFlags changedStates() override
     {
         return StateFlags(StencilState) | ColorState | BlendState;
     }
 
-    void render(const RenderState& state) Q_DECL_OVERRIDE
+    void render(const RenderState& state) override
     {
         renderInternal(state.projectionMatrix);
     }
